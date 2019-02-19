6dd588846a7a831a6604101d97e4830de4a35366
Ulrich Schmidt <admin@deruli.de>
2019-02-19 23:00:06 +0100

    fix html

--- src/content/modules/convert_to_utf8mb4/templates/form.php
+++ src/content/modules/convert_to_utf8mb4/templates/form.php
@@ -4,7 +4,7 @@
 <form action="#" method="get">
 	<div id="convert-container">
 		<p><?php translate("convert_to_utf8mb4_help_text")?></p>
-		<button type="button" id="btn_start_conversion"><i class="fas fa-exchange-alt"></i> <?php translate("start_conversion");?>"></button>
+		<button type="button" id="btn_start_conversion" class="btn btn-primary"><i class="fas fa-exchange-alt"></i> <?php translate("start_conversion");?></button>
 	</div>
 </form>
 <script type="text/javascript"
